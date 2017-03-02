46a7e07f58c5b9e40694a5ec44151bfb9f584bf0
AIZAWA Hina <hina@bouhime.com>
2017-03-03 02:02:28 +0900

    init
    Signed-off-by: AIZAWA Hina <hina@bouhime.com>

---
+++ src/UbuntuCondensedFontAsset.php
@@ -0,0 +1,11 @@
+<?php
+namespace jp3cki\yii2\ubuntu;
+
+use yii\web\AssetBundle;
+
+class UbuntuCondensedFontAsset extends AssetBundle
+{
+    public $css = [
+        'https://fonts.googleapis.com/css?family=Ubuntu+Condensed',
+    ];
+}
---
+++ src/UbuntuMonoFontAsset.php
@@ -0,0 +1,11 @@
+<?php
+namespace jp3cki\yii2\ubuntu;
+
+use yii\web\AssetBundle;
+
+class UbuntuMonoFontAsset extends AssetBundle
+{
+    public $css = [
+        'https://fonts.googleapis.com/css?family=Ubuntu+Mono',
+    ];
+}
